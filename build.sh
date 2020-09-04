b76175dd04baace2327518a78536d35bac6c7e2b
David Evans <dave@evans.io>
2020-09-04 17:58:05 +0100

    Switch to manylinux2014 'cos it's newer

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec docker run --rm -it -v $(pwd):/io quay.io/pypa/manylinux2010_x86_64 /io/inner_build.sh
+exec docker run --rm -it -v $(pwd):/io quay.io/pypa/manylinux2014_x86_64 /io/inner_build.sh
